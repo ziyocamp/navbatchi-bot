5d451a9fa3870407e7a9332a1c99f8d3d1a04a18
djumanov <djumanovdev@gmail.com>
2025-08-22 16:53:31 +0500

    start

--- handlers.py
+++ handlers.py
@@ -1,6 +1,21 @@
-from telegram import Update
+from telegram import (
+    Update, 
+    ReplyKeyboardMarkup, KeyboardButton,
+)
 from telegram.ext import CallbackContext
 
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text("salom")
+    update.message.reply_text(
+        "salom navbatchi botga xush kelibsiz",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton("Bugungi navbatchi"), 
+                    KeyboardButton("Xaftalik navbatchilar ro'yxati"),
+                    KeyboardButton("Navbatchilik uchun kunni tanlash"),
+                ]
+            ],
+            resize_keyboard=True
+        )
+    )
